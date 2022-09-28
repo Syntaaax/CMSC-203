a5560db4e5359bba5d507ef3eca9897d118f4853
Francine <112456101+Syntaaax@users.noreply.github.com>
2022-09-28 22:57:27 +0800

    Add files via upload

--- CMSC203_ACTIVITY3_MARTIZANO/TestCase.java
+++ CMSC203_ACTIVITY3_MARTIZANO/TestCase.java
@@ -3,25 +3,60 @@ package CMSC203_ACTIVITY3_MARTIZANO;
 import java.util.Scanner;
 
 public class TestCase {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
+        boolean done = false;
 
-        System.out.print("No. of test cases: ");
-        int t = sc.nextInt();
-        while (t > 0) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            System.out.println(x == y ? "ANY" : x < y ? "FIRST" : "SECOND");
-            t--;
+        do {
+            System.out.print("No. of test cases: ");
+            int t = sc.nextInt();
+
+            if (1 <= t && t <= 10) {
+                String inputs[] = new String[t];
+
+                for (int i = 0; i < t; i++) {
+                    sc = new Scanner(System.in);
+                    inputs[i] = sc.nextLine();
+                }
+
+                System.out.println("\nResult:");
+                System.lineSeparator();
+
+                for (String input : inputs)
+                    System.out.println(processInput(input));
+
+                done = true;
+            } else
+                System.out.println("Enter a number from 1-10");
+
+        } while (!done);
+        sc.close();
+    }
+
+    private static String processInput(String input) {
+        String[] inputs = input.split("\\s+");
+
+        if (inputs.length != 2)
+            return "INVALID INPUT";
+
+        try {
+            int x = Integer.parseInt(inputs[0]);
+            int y = Integer.parseInt(inputs[1]);
+
+            if (x < 1 || x > 1000
+                    || y < 1 || y > 1000)
+                return "INVALID INPUT";
+
+            if (x < y)
+                return "FIRST";
+
+            else if (x > y)
+                return "SECOND";
+
+            else
+                return "ANY";
+        } catch (Exception e) {
+            return "INVALID INPUT";
         }
-        System.out.println("\nNo. of test cases: 3");
-        System.out.println("95 360");
-        System.out.println("500 500");
-        System.out.println("1001 1");
-
-        System.out.println("\nResult: ");
-        System.out.println("FIRST");
-        System.out.println("ANY");
-        System.out.println("SECOND");
     }
 }
